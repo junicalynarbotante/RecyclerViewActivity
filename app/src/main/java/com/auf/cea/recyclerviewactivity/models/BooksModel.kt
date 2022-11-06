48a6dc5ec4560ea14dbf1fa162a09b8c7a740625
Japhet Ong <japhetlee.dev@gmail.com>
2022-11-06 13:30:42 +0800

    Added BookList and BooksModel.kt

---
+++ app/src/main/java/com/auf/cea/recyclerviewactivity/models/BooksModel.kt
@@ -0,0 +1,9 @@
+package com.auf.cea.recyclerviewactivity.models
+
+data class BooksModel(
+    var name: String,
+    var author: String,
+    var shortDescription: String,
+    var description: String,
+    var datePublished: String,
+)
